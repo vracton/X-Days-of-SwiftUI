a67b6ee1ed8dc8233bc248b6cb07be21b936c8a3
vracto <sonit.sahoo@icloud.com>
2025-06-06 11:56:44 -0500

    BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sonit Sahoo on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRestTests/BetterRestTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestTests.swift
+//  BetterRestTests
+//
+//  Created by Sonit Sahoo on 6/5/25.
+//
+
+import Testing
+@testable import BetterRest
+
+struct BetterRestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
